3da1fbf411c834c494e68e8408c76cfe25785920
laerkeloenborg <lalo0001@stud.kea.dk>
2025-03-27 10:47:50 +0100

    WishRepo created

--- src/main/java/org/example/wishlist/repository/WishRepository.java
+++ src/main/java/org/example/wishlist/repository/WishRepository.java
@@ -1,4 +1,55 @@
 package org.example.wishlist.repository;
 
+import org.example.wishlist.model.Wish;
+import org.example.wishlist.model.Wishlist;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
 public class WishRepository {
+private JdbcTemplate jdbcTemplate;
+
+
+public WishRepository(JdbcTemplate jdbcTemplate){
+    this.jdbcTemplate = jdbcTemplate;
+}
+
+public void getAllWishes(){
+
+}
+
+public void getAllWishlist(){
+
+}
+
+public void addWish(Wish wish){
+
+}
+
+public void addWishlist(Wishlist wishlist){
+
+}
+
+public void deleteWish (String name){
+
+}
+
+public void deleteWishlist (String name){
+
+}
+
+public void updateWish(String name, Wish newWish){
+
+}
+
+public void updateWishlist(String name, Wishlist newWishlist){
+
+}
+
+
+
+
+
 }
