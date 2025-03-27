1e36df50ef2ce7824df98dc0842064ee27067a1a
Mads Engstrøm <mads61q5@stud.kea.dk>
2025-03-27 10:01:20 +0100

    Wish klasse lavet

---
+++ src/main/java/org/example/wishlist/model/Wish.java
@@ -0,0 +1,48 @@
+package org.example.wishlist.model;
+
+public class Wish {
+
+    private String name;
+    private String description;
+    private int price;
+    private String link;
+
+    public Wish(String name, String description, int price, String link) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
