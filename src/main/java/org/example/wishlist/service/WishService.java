6bdd80840376df8a9b380377b81fb8379c660ac0
CamillaEva <caha0007@stud.kea.dk>
2025-04-01 09:52:07 +0200

    rettelser i service, så den også tager wishlist repo med sig

--- src/main/java/org/example/wishlist/service/WishService.java
+++ src/main/java/org/example/wishlist/service/WishService.java
@@ -3,6 +3,7 @@ package org.example.wishlist.service;
 import org.example.wishlist.model.Wish;
 import org.example.wishlist.model.Wishlist;
 import org.example.wishlist.repository.WishRepository;
+import org.example.wishlist.repository.WishlistRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,16 +11,22 @@ import java.util.List;
 @Service
 public class WishService {
     private final WishRepository wishRepository;
+    private final WishlistRepository wishlistRepository;
 
-    public WishService(WishRepository wishRepository) {
+    public WishService(WishRepository wishRepository, WishlistRepository wishlistRepository) {
         this.wishRepository = wishRepository;
+        this.wishlistRepository = wishlistRepository;
     }
 
     public List<Wishlist> getAllWishLists() {
-        return wishRepository.getAllWishList();
+        return wishlistRepository.getAllWishList();
     }
 
     public void updateWish(Wish wish) {
         wishRepository.updateWish(wish);
+    }
+
+    public void updateWishlist(String name, Wishlist wishlist){
+        wishlistRepository.updateWishlist(name, wishlist);
     }
 }
